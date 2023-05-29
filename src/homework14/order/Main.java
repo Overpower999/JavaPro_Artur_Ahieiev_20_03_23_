06e4a4fd52ff2bf564ba39165cba091455379bcd
Artur Ahieiev <ageev20111@gmail.com>
2023-05-29 19:11:25 +0300

    Homework 14

---
+++ src/homework14/order/Main.java
@@ -0,0 +1,27 @@
+package homework14.order;
+
+public class Main {
+    public static void main(String[] args) {
+        CoffeeOrderBoard cob = new CoffeeOrderBoard();
+
+        cob.add("Olga");
+        cob.add("Dmitro");
+        cob.add("Oleg");
+        cob.add("Daria");
+        cob.add("Ivan");
+
+        cob.draw();
+        System.out.println("Deliver first");
+        cob.deliver();
+        cob.draw();
+
+        System.out.println("Deliver one of..");
+        cob.deliver(10);
+        cob.draw();
+
+        System.out.println("Deliver first");
+        cob.deliver();
+        cob.draw();
+
+    }
+}
